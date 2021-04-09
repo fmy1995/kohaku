f53f4ffdd57d3ea27b53d098adfa7e7b4b0760be
fmy1995 <fmy.kymz444@gmail.com>
2021-04-09 10:55:15 +0000

    image add

--- kohaku/app/controllers/posts_controller.rb
+++ kohaku/app/controllers/posts_controller.rb
@@ -1,13 +1,37 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def index
+    @posts = Post.page(params[:page]).reverse_order
   end
 
   def show
+   @post = Post.find(params[:id])
+   @post_comment = PostComment.new
   end
 
-  def new
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
-
-  def edit
-  end
+  
+  private
+def post_params
+params.require(:post).permit(:title, :body, :image)
+end
+  
+  
 end
--- kohaku/db/schema.rb
+++ kohaku/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_120950) do
+ActiveRecord::Schema.define(version: 2021_04_09_103535) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_08_120950) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
