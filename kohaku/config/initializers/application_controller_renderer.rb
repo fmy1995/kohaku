39ba7561105ddaa52cb93203c70fd57b45b275c7
fmy1995 <fmy.kymz444@gmail.com>
2021-04-12 04:34:13 +0000

    top image

--- kohaku/config/initializers/application_controller_renderer.rb
+++ kohaku/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd0dc8479cb1f4c920619f442937648a18629b5da02eaaf31020b5d1941dad84d3aa37bb77282a2145f42b14b865bdba57f9d07fc27933683e34ec496cb872879'
