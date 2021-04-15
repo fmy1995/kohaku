7c8603fe921f78c77f76eddc42c2e23f0bdef98a
fmy1995 <fmy.kymz444@gmail.com>
2021-04-15 11:36:18 +0000

    top

--- kohaku/app/controllers/homes_controller.rb
+++ kohaku/app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
 
 def top
-  @posts = Post.page(params[:page]).reverse_order    
+  @posts = Post.page(params[:page]).reverse_order
+  @genres = Genre.page(params[:page]).reverse_order
 end
 
 end
